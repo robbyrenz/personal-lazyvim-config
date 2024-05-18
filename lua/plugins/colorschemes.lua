9f214fc55c6640ab0d37b5b5aaaf08eb736caafe
Robby <nxgrenz@gmail.com>
2024-05-18 09:34:28 -0500

    add new theme called 'cyberdream'
    I also made 'catppuccin-frappe' as my default nvim theme.

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -12,21 +12,29 @@ return {
     },
   },
   { "sainnhe/sonokai" },
+  {
+    "scottmckendry/cyberdream.nvim",
+    opts = {
+      lazy = false,
+      priority = 1000,
+    },
+  },
 
   -- Configure LazyVim to load the below colorscheme on startup
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox-material",
+      -- Uncomment the below if you want LazyVim to load gruvbox-material on startup
+      -- colorscheme = "gruvbox-material",
 
-      -- Uncomment the below if you want LazyVim to load rose-pine-moon
+      -- Uncomment the below if you want LazyVim to load rose-pine-moon on startup
       -- colorscheme = "rose-pine-moon",
 
       -- Uncomment the below if you want LazyVim to load tokyonight
       -- colorscheme = "tokyonight",
 
-      -- Uncomment the below if you want LazyVim to load tokyonight
-      -- colorscheme = "catppuccin",
+      -- Uncomment the below if you want LazyVim to load catppuccin-frappe on startup
+      colorscheme = "catppuccin-frappe",
     },
   },
 }
