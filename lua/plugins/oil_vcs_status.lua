a16a9af844a15cea45693bfa9ec7ea5044d204fb
Robby <nxgrenz@gmail.com>
2024-05-19 11:11:00 -0500

    add comment to a link to GitHub

--- lua/plugins/oil_vcs_status.lua
+++ lua/plugins/oil_vcs_status.lua
@@ -1,4 +1,5 @@
 -- TODO: customize the icons to use symbols instead of letters!
+-- https://github.com/SirZenith/oil-vcs-status?tab=readme-ov-file#symbol-customization
 return {
   "SirZenith/oil-vcs-status",
 }
