d866041f58b2df87f79241703c13d9f5c636afbe
Robby <nxgrenz@gmail.com>
2024-05-26 09:43:01 -0500

    set Oil to open a floating pane by default
    Also show hidden files and directories by default (on Oil).

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,20 +1,34 @@
 return {
-  "stevearc/oil.nvim",
-  keys = {
-    {
-      "-",
-      "<CMD>Oil<CR>",
-      desc = "Open parent directory",
+  {
+    "stevearc/oil.nvim",
+    keys = {
+      -- {
+      -- Open in a separate buffer
+      --   "-",
+      --   "<CMD>Oil<CR>",
+      --   desc = "Open parent directory",
+      -- },
+      {
+        -- Open in a floating pane
+        "-",
+        function()
+          require("oil").toggle_float()
+        end,
+        desc = "Open parent directory",
+      },
     },
+    config = function()
+      require("oil").setup({
+        keymaps = {
+          ["<BS>"] = "actions.parent",
+        },
+        win_options = {
+          signcolumn = "number",
+        },
+        view_options = {
+          show_hidden = true,
+        },
+      })
+    end,
   },
-  config = function()
-    require("oil").setup({
-      keymaps = {
-        ["<BS>"] = "actions.parent",
-      },
-      win_options = {
-        signcolumn = "number",
-      },
-    })
-  end,
 }
