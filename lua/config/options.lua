ae882158aec8ad3bc61a91a6cd30d6e830333b34
Robby <nxgrenz@gmail.com>
2024-08-10 12:06:47 -0500

    Add EOL character!
    Woohoo! I finally found this configuration!

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Just a couple of options for the type of character shown at the end of each line
+-- Comment/uncomment them as you see fit
+-- vim.opt.listchars:append("eol:↴")
+vim.opt.listchars = { eol = "⏎" }
+-- vim.opt.listchars = "eol:↲"
+-- vim.opt.listchars = "eol:↵"
