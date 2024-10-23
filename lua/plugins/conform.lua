9871e895c4131472994a14bfe48327ac511be076
Robby <nxgrenz@gmail.com>
2024-10-23 08:00:33 -0500

    Conform now exclusively uses deno_fmt to format TypeScript and JavaScript files

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,8 +3,10 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        java = { "google-java-format" },
+        typescript = { "deno_fmt" },
+        javascript = { "deno_fmt" },
         markdown = { "markdownlint" },
+        java = { "google-java-format" },
       },
     },
   },
