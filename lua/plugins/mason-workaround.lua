accc3ad38dc3bba90600c6896997c5d18733d27f
Robby <nxgrenz@gmail.com>
2025-09-21 09:47:25 -0500

    feat!(LazyVim): update LazyVim and it's plugins
    This is a big update! I disabled the mason workaround because the
    corresponding PR fix was merged into the main branch! Check the diff of
    this PR for the link to the fix!
    
    There has also been a couple of mini plugins that had their origins
    changed!

--- lua/plugins/mason-workaround.lua
+++ lua/plugins/mason-workaround.lua
@@ -1,10 +1,18 @@
 -- https://github.com/mason-org/mason-lspconfig.nvim/issues/543#issuecomment-2856464693 (where I first heard that this is a LazyVim issue, and a link to this workaround)
 -- https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2856227817 (where I actually got this code snippet)
-return {
-  { "mason-org/mason.nvim", version = "^1.0.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
-}
 
 -- Note: there already is a PR that fixes this: https://github.com/LazyVim/LazyVim/pull/6053
 -- But we will have to wait until Folke comes back from vacation hehe: https://github.com/LazyVim/LazyVim/pull/6053#issuecomment-3116580706
 -- Once this breaking change with Mason is fixed, this file will no longer be needed
+
+-- By-passing the changes in this file, as Folke came back, and merged the aforementioned PR:
+--   * https://github.com/LazyVim/LazyVim/pull/6053#issuecomment-3290687150
+--   * https://github.com/LazyVim/LazyVim/pull/6053#issuecomment-3291400147
+if true then
+  return {}
+end
+
+return {
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+}
